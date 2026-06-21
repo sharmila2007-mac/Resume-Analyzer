7d51c192fc0f0dd1f44bc6a110181a53fd22bf28
Sharmila Devi Annepu <sharmiladeviannepu@Sharmila-ka-MacBook-Air.local>
2026-06-21 16:14:43 +0530

    Initial Resume Analyzer Project

---
+++ skills.py
@@ -0,0 +1,19 @@
+skills_list = [
+    "Python",
+    "Java",
+    "C",
+    "C++",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "SQL",
+    "Flask",
+    "Django",
+    "Machine Learning",
+    "Data Science",
+    "Git",
+    "GitHub",
+    "React",
+    "Node.js",
+    "MongoDB"
+]
